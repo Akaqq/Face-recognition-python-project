dd10ca659c2488d29659e63e4f7f5568660be949
Akaqq <k.itfc88@gmail.com>
2024-03-09 03:23:30 +0300

    update

--- main.py
+++ main.py
@@ -3,18 +3,7 @@ import numpy as np
 import face_recognition
 import os
 
-path = 'persons'
-images = []
-classNames = []
-personsList = os.listdir(path)
-
-for cl in personsList:
-    curPersonn = cv2.imread(f'{path}/{cl}')
-    images.append(curPersonn)
-    classNames.append(os.path.splitext(cl)[0])
-print(classNames)
-
-def findEncodeings(image):
+def findEncodeings(images):
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -22,14 +11,24 @@ def findEncodeings(image):
         encodeList.append(encode)
     return encodeList
 
-encodeListKnown = findEncodeings(images)
-print('Encoding Complete.')
+def main():
+    path = 'persons'
+    images = []
+    classNames = []
+    personsList = os.listdir(path)
 
-cap = cv2.VideoCapture(1)
+    for cl in personsList:
+        curPersonn = cv2.imread(f'{path}/{cl}')
+        images.append(curPersonn)
+        classNames.append(os.path.splitext(cl)[0])
+    print(classNames)
 
-while True:
-    _, img = cap.read()
+    encodeListKnown = findEncodeings(images)
+    print('Encoding Complete.')
 
+    # Load the image for face detection
+    image_path = 'test_image.jpg'
+    img = cv2.imread(image_path)
     imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -39,9 +38,9 @@ while True:
     for encodeface, faceLoc in zip(encodeCurentFrame, faceCurentFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeface)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeface)
-        matchIndex = np.argmin(faceDis)
+        matchIndices = [i for i, match in enumerate(matches) if match]
 
-        if matches[matchIndex]:
+        for matchIndex in matchIndices:
             name = classNames[matchIndex].upper()
             print(name)
             y1, x2, y2, x1 = faceLoc
@@ -50,5 +49,9 @@ while True:
             cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,0,255), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
 
-    cv2.imshow('Face Recogntion', img)
-    cv2.waitKey(1)
+    cv2.imshow('Face Recognition', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
